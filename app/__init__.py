8cd3dc3148baddbca4396c2302501fc99e5770ed
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2022-07-04 23:08:03 +0200

    Fix code for pylint

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+"""
+App : chess UI and IA
+"""
+from pkgutil import extend_path
+
+# Permet d'éviter les erreus de pylint
+__path__ = extend_path(__path__, __name__)
--- app/tests/test_version.py
+++ app/tests/test_version.py
@@ -1,5 +1,14 @@
+"""
+Simple test, to trigger pipelines
+Will be removed
+"""
+
 import app.src
 
 
 def test_version():
+    """
+    Test le numéro de version du projet
+    :return: None
+    """
     assert app.src.__version__ == "0.1.0"
