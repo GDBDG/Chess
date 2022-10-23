9f734f0dc966b9f8c367411784ce1d7ccbec9e67
gdbdg <guillaume.degranrut@gmail.com>
2022-10-23 16:56:21 +0200

    refacto: refacto game state,
    function for each rule

--- app/src/model/states/game_state.py
+++ app/src/model/states/game_state.py
@@ -4,6 +4,7 @@ Associated methods, (state update and getter)
 """
 from app.src.model.classes.const.color import Color
 from app.src.model.events.moves.pawn_move import PawnMove
+from app.src.model.states.board import Board
 from app.src.model.states.game_historic import GameHistoric
 
 
@@ -27,11 +28,11 @@ class GameState:
         self.fifty_counter = 0
         self.player = Color.WHITE
 
-    def update_state(self, game_historic: GameHistoric, capture: bool):
+    def fifty_move_rule(self, game_historic: GameHistoric, capture: bool):
         """
         Update the counter for the '50-moves' rule
         @param game_historic:
-        @param capture: boolean value if the last moves was a capture
+        @param capture: boolean, if the last move is a capture or not
         @return:
         """
         if capture or isinstance(game_historic.move_historic[-1], PawnMove):
@@ -40,11 +41,34 @@ class GameState:
             self.fifty_counter += 1
             if self.fifty_counter == 100:
                 self.state = GameState.DRAW
+
+    def three_fold_rule(self, game_historic: GameHistoric):
+        """
+        Update the three-fold state
+        @return:
+        """
+        if 3 in game_historic.config_historic.values():
+            self.state = GameState.DRAW
+
+    def dead_position_rule(self, board: Board):
+        """
+        Update the state for the dead positions rule
+        @param board:
+        @return:
+        """
+
+    def update_state(self, game_historic: GameHistoric, capture: bool):
+        """
+        Update the state for all rules
+        @param game_historic:
+        @param capture: boolean value if the last moves was a capture
+        @return:
+        """
+        self.fifty_move_rule(game_historic, capture)
+        self.three_fold_rule(game_historic)
         # pylint: disable=W0511
         # Dead position
         # TODO
-        # Threefold rule
-        if 3 in game_historic.config_historic.values():
-            self.state = GameState.DRAW
+
         # Update the player who has to play
         self.player = Color.BLACK if self.player == Color.WHITE else Color.WHITE
